86c5c6b7e489e5dd3aecd4ff2a1bab891b2583fd
Thirup2 <246946332@qq.com>
2022-06-01 23:33:30 +0800

    LeetCode 专题突破 ( 数组类算法 02 )

--- 数据结构与算法/LeetCode 专项突破/01 数组类算法/02 移除元素/02.c
+++ 数据结构与算法/LeetCode 专项突破/01 数组类算法/02 移除元素/02.c
@@ -1,4 +1,15 @@
 int removeElement(int *nums, int numsSize, int val)
 {
-
+    int i = 0, j = 0;
+    int count = 0;
+    while (j < numsSize) {
+        if (nums[j] == val) {
+            ++j;
+        } else {
+            nums[i] = nums[j];
+            ++i; ++j;
+            ++count;
+        }
+    }
+    return count;
 }
