8655861b474617a57414dbfd00751e306257cf1b
Thirup2 <246946332@qq.com>
2022-06-02 23:59:11 +0800

    LeetCode 专项突破 ( 数组类算法 03)

--- 数据结构与算法/LeetCode 专项突破/01 数组类算法/03 删除排序数组中的重复项/03.c
+++ 数据结构与算法/LeetCode 专项突破/01 数组类算法/03 删除排序数组中的重复项/03.c
@@ -1,4 +1,11 @@
 int removeDuplicates(int *nums, int numsSize)
 {
-
+    int i = 0, j = 1;
+    for (; i + 1 < numsSize; i++) {
+        if (nums[i] != nums[i + 1]) {
+            nums[j] = nums[i + 1];
+            j++;
+        }
+    }
+    return (numsSize == 0) ? 0 : j;
 }
---
+++ 数据结构与算法/LeetCode 专项突破/01 数组类算法/04 删除排序数组中的重复项 II/04.c
@@ -0,0 +1,4 @@
+int removeDuplicates(int *nums, int numsSize)
+{
+
+}
